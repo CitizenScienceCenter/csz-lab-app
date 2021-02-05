83b424ecb147d0e9c80909c2e87fa9657be57311
Andres Dorado <andres.dorado90@gmail.com>
2021-02-05 11:28:52 +0100

    hide Forum temporary. While is defined the way to implement

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -19,7 +19,8 @@
             <b-nav-item :to="{ name: 'home' }">{{ $t('app-vue-navigation-home') }}</b-nav-item>
             <b-nav-item :to="{ name: 'discover' }">{{ $t('app-vue-navigation-discover') }}</b-nav-item>
             <b-nav-item :to="{ name: 'about' }">{{ $t('app-vue-navigation-about') }}</b-nav-item>
-            <b-nav-item :to="{ name: 'forum' }">Forum</b-nav-item>
+            <!-- TODO: uncomment when Forum for each particular projects is done -->
+            <!-- <b-nav-item :to="{ name: 'forum' }">Forum</b-nav-item> -->
           </b-navbar-nav>
           
           <b-navbar-nav>
