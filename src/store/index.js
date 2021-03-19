cb5273e2d299e46601d90963ca6a51e2f12b6931
Andres Dorado <andres.dorado90@gmail.com>
2021-03-19 19:26:12 +0100

    GDPR modified
    instead using localstorage the vue-persistence with encription is implemented

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,23 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import createLogger from 'vuex/dist/logger'
+import Vue from "vue";
+import Vuex from "vuex";
+import createLogger from "vuex/dist/logger";
 
 // list of modules
-import notification from './modules/notification'
-import project from './modules/project'
-import user from './modules/user'
-import task from './modules/task'
-import osm from './modules/osm'
-import settings from './modules/settings'
-import snakes from './modules/snakes'
-import comments from './modules/comments'
+import notification from "./modules/notification";
+import project from "./modules/project";
+import user from "./modules/user";
+import task from "./modules/task";
+import osm from "./modules/osm";
+import settings from "./modules/settings";
+import snakes from "./modules/snakes";
+import comments from "./modules/comments";
+import createPersistedState from "vuex-persistedstate";
+import SecureLS from "secure-ls";
+var ls = new SecureLS({ isCompression: false });
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-const debug = process.env.NODE_ENV !== 'production'
+const debug = process.env.NODE_ENV !== "production";
 
 export default new Vuex.Store({
   modules: {
@@ -28,5 +31,24 @@ export default new Vuex.Store({
     comments
   },
   strict: debug,
-  plugins: debug ? [createLogger()] : []
-})
+  plugins: debug
+    ? [
+        createLogger(),
+        createPersistedState({
+          storage: {
+            getItem: key => ls.get(key),
+            setItem: (key, value) => ls.set(key, value),
+            removeItem: key => ls.remove(key)
+          }
+        })
+      ]
+    : [
+        createPersistedState({
+          storage: {
+            getItem: key => ls.get(key),
+            setItem: (key, value) => ls.set(key, value),
+            removeItem: key => ls.remove(key)
+          }
+        })
+      ]
+});
