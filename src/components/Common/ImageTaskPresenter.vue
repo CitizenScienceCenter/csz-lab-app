89b03ba46f31575efd0cf163105877c06b335965
Andres Dorado <andres.dorado90@gmail.com>
2021-03-23 14:16:46 +0100

    disable scroll to zoom in croppa component for images
    replaced for input range

--- src/components/Common/ImageTaskPresenter.vue
+++ src/components/Common/ImageTaskPresenter.vue
@@ -9,12 +9,16 @@
         ></b-skeleton-img>
       </template>
       <beauty-img
+        v-model="imgObject"
         class="beauty-image"
         ref="beauty-image"
         :height="getSize"
         canvas-color="#e9e9e9"
         :prevent-white-space="true"
         :show-remove-button="false"
+        :disable-drag-and-drop="true"
+        :disable-click-to-choose="true"
+        :disable-scroll-to-zoom="true"
         :show-loading="true"
         :loading-size="10"
         :zoom-speed="5"
@@ -24,11 +28,28 @@
         initial-position="center center"
         initial-size="cover"
         auto-sizing
+        @new-image-drawn="onNewImage"
+        @zoom="onZoom"
       >
         <img v-if="info.url" slot="initial" :src="info.url" />
         <img v-else slot="initial" :src="info.link_raw" />
       </beauty-img>
-      <div class="buttons-img">
+      <div class="buttons-img ">
+        <transition name="fade">
+          <b-form-input
+            v-model="currentZoom"
+            type="range"
+            :min="minZoom"
+            :max="maxZoom"
+            step=".001"
+            v-if="isZoom"
+            orient="vertical"
+            @input="onSliderChange"
+          ></b-form-input>
+        </transition>
+        <button class="button-img mb-3" @click="toogleZoom()">
+          <i class="fas fa-binoculars fa-lg"></i>
+        </button>
         <button
           @click="
             pybossa.showModal('image', info.url ? info.url : info.link_raw)
@@ -38,7 +59,7 @@
           <i class="fas fa-expand fa-lg"></i>
         </button>
       </div>
-    </b-skeleton-wrapper>    
+    </b-skeleton-wrapper>
   </div>
 </template>
 
@@ -46,6 +67,15 @@
 import { getWidthScreen } from "@/helper.js";
 
 export default {
+  data() {
+    return {
+      isZoom: false,
+      imgObject: {},
+      currentZoom: 0,
+      minZoom: 0,
+      maxZoom: 0
+    };
+  },
   props: {
     pybossa: Object,
     info: Object,
@@ -61,6 +91,24 @@ export default {
         return 450;
       }
       return 550;
+    }
+  },
+  methods: {
+    toogleZoom() {
+      this.isZoom = !this.isZoom;
+    },
+    onNewImage() {
+      this.isZoom = false;
+      this.currentZoom = this.imgObject.scaleRatio;
+      this.minZoom = this.imgObject.scaleRatio * 1;
+      this.maxZoom = this.imgObject.scaleRatio * 4;
+    },
+    onSliderChange(value) {
+      var increment = value;
+      this.imgObject.scaleRatio = +increment;
+    },
+    onZoom() {
+      this.currentZoom = this.imgObject.scaleRatio;
     }
   }
 };
@@ -82,11 +130,31 @@ export default {
     background: rgba(255, 255, 255, 0.6);
     height: 2.5em;
     width: 2.5em;
+    box-shadow: 1px 1px 5px $black;
   }
   .button-img:hover {
     background: rgba(255, 255, 255, 0.9);
-    height: 2.7em;
-    width: 2.7em;
+    height: 2.6em;
+    width: 2.6em;
   }
+}
+// Slide vertical style
+input[type="range"][orient="vertical"] {
+  -webkit-appearance: none;
+  height: 250px;
+  width: 10px;
+  border-radius: 5px;
+  background: #ccc;
+  outline: none;
+  writing-mode: bt-lr; /* IE */
+  -webkit-appearance: slider-vertical; /* WebKit */
+  box-shadow: 1px 1px 5px $black;
+}
+// Slide transition
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
