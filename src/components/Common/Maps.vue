89145ce6b45056638eb9b1aace15cee8346463a6
Andres Dorado <andres.dorado90@gmail.com>
2021-03-25 15:18:36 +0100

    Geosearch and add-remove makers
    pending edition mode control

--- src/components/Common/Maps.vue
+++ src/components/Common/Maps.vue
@@ -1,56 +1,76 @@
 <template>
   <div class="maps">
-    <l-map
+    <v-map
       style="height: 80%; width: 100%"
+      ref="maps"
       :zoom="zoom"
       :center="center"
       @update:zoom="zoomUpdated"
       @update:center="centerUpdated"
       @update:bounds="boundsUpdated"
+      :options="{ zoomControl: false }"
+      @click="addMarker"
     >
-      <l-tile-layer :url="url"></l-tile-layer>
+      <v-geosearch :options="geosearchOptions" ref="geosearch"></v-geosearch>
+      <v-tile-layer :url="url" :attribution="attribution"></v-tile-layer>
+      <v-control-zoom position="bottomright"></v-control-zoom>
+
+      <!-- <v-control-zoom position="bottomright"></v-control-zoom> -->
       <v-marker-cluster>
         <v-marker
           v-for="(location, index) in locations"
           :key="index"
           :lat-lng="location"
+          @click="removeMarker(index)"
         ></v-marker>
       </v-marker-cluster>
-    </l-map>
+    </v-map>
   </div>
 </template>
 
 <script>
-import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+import { LMap, LTileLayer, LMarker, LControlZoom } from "vue2-leaflet";
+import { OpenStreetMapProvider } from "leaflet-geosearch";
+import VGeosearch from "vue2-leaflet-geosearch";
 import { Icon } from "leaflet";
 
 import Vue2LeafletMarkerCluster from "vue2-leaflet-markercluster";
 
 const DummyLocations = [
   { lat: 2.4471708623464177, lng: -76.60543462886939 },
-  { lat: 2.4471708623464177, lng: -76.60543462886939 },
+  { lat: 2.4071708623464177, lng: -76.60543462886939 },
   { lat: 2.4471708623464177, lng: -75.60543462886939 },
-  { lat: 47.3937203599115, lng: 8.544738061411797 },
-  { lat: 47.3744092665564, lng: -98.550949011575966 },
-  { lat: 40.857687895771015, lng: -90.143336159214287 },  
+  { lat: 45.3937203599115, lng: 8.544738061411797 },
+  { lat: 47.3744092665564, lng: -95.550949011575966 },
+  { lat: 40.857687895771015, lng: -90.143336159214287 },
   { lat: 47.3937203599115, lng: 8.544738061411797 },
   { lat: 47.3744092665564, lng: -98.550949011575966 }
 ];
 
 export default {
   components: {
-    LMap,
-    LTileLayer,
+    "v-map": LMap,
+    "v-tile-layer": LTileLayer,
     "v-marker": LMarker,
-    "v-marker-cluster": Vue2LeafletMarkerCluster
+    "v-marker-cluster": Vue2LeafletMarkerCluster,
+    "v-control-zoom": LControlZoom,
+    VGeosearch
   },
   data() {
     return {
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
       zoom: 3,
       center: [47.41322, -1.219482],
       bounds: null,
-      locations: []
+      locations: [],
+      geosearchOptions: {
+        provider: new OpenStreetMapProvider(),
+        autoClose: true,
+        keepResult: false
+      },
+      isEdition: false
     };
   },
   methods: {
@@ -63,6 +83,26 @@ export default {
     boundsUpdated(bounds) {
       this.bounds = bounds;
     },
+    removeMarker(index) {
+      console.log("jajajaja");
+      this.locations.splice(index, 1);
+    },
+    addMarker(event) {
+      this.isEdition = !document
+        .getElementsByClassName("geosearch")[0]
+        .classList.contains("active");
+      if (
+        (event.containerPoint.x >= 10 && event.containerPoint.x <= 40) ||
+        (event.containerPoint.y >= 10 && event.containerPoint.y <= 40)
+      ) {
+        this.isEdition = false;
+        return;
+      }
+      if (this.isEdition) {
+        this.locations.push(event.latlng);
+      }
+    },
+
     setData() {
       this.locations = DummyLocations.map(x => [x.lat, x.lng]);
       const [minLat, maxLat, minLng, maxLng] = [
@@ -88,9 +128,10 @@ export default {
 
 <style lang="scss" scoped>
 .maps {
-  height: 75vh;
-  width: 75vh;
+  height: 80vh;
+  width: 100vw;
   position: relative;
   margin: auto;
+  padding-bottom: 100px;
 }
 </style>
