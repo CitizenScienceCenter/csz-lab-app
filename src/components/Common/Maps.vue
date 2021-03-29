5522be81f48dbc3eda8ad6d1995cf04b62adbc5b
Andres Dorado <andres.dorado90@gmail.com>
2021-03-30 01:03:18 +0200

    polygon drawing implementation
    edition mode implementation

--- src/components/Common/Maps.vue
+++ src/components/Common/Maps.vue
@@ -9,60 +9,104 @@
       @update:center="centerUpdated"
       @update:bounds="boundsUpdated"
       :options="{ zoomControl: false }"
-      @click="addMarker"
+      @click="clickOnMap"
     >
       <v-geosearch :options="geosearchOptions" ref="geosearch"></v-geosearch>
       <v-tile-layer :url="url" :attribution="attribution"></v-tile-layer>
       <v-control-zoom position="bottomright"></v-control-zoom>
 
-      <!-- <v-control-zoom position="bottomright"></v-control-zoom> -->
       <v-marker-cluster>
         <v-marker
           v-for="(location, index) in locations"
-          :key="index"
+          :key="`marker${index}`"
           :lat-lng="location"
-          @click="removeMarker(index)"
-        ></v-marker>
+          @click="clickOnMarker(location, index)"
+        >
+        </v-marker>
+        <v-popup
+          v-for="(location, index) in locations"
+          :key="`popup${index}`"
+          :lat-lng="location"
+          :content="getLocation(location)"
+        />
       </v-marker-cluster>
+
+      <v-polygon
+        :lat-lngs="polygon.latlngs"
+        :color="polygon.color"
+        @click="clickOnPoligon"
+      ></v-polygon>
+
+      <v-control :position="'bottomleft'" class="custom-buttons">
+        <button
+          class="custom-button mb-1"
+          @click="toggleEditionMode('marker')"
+          :class="{ active: toolbar.tools.marker }"
+          :disabled="!isEditable"
+        >
+          <i class="fas fa-map-marker-alt fa-2x"></i>
+        </button>
+        <button
+          class="custom-button mb-2"
+          @click="toggleEditionMode('polygon')"
+          :class="{ active: toolbar.tools.polygon }"
+          :disabled="!isEditable"
+        >
+          <i class="fas fa-draw-polygon fa-2x"></i>
+        </button>
+      </v-control>
     </v-map>
   </div>
 </template>
 
 <script>
-import { LMap, LTileLayer, LMarker, LControlZoom } from "vue2-leaflet";
+import {
+  LMap,
+  LTileLayer,
+  LMarker,
+  LControlZoom,
+  LControl,
+  LPopup,
+  LPolygon
+} from "vue2-leaflet";
 import { OpenStreetMapProvider } from "leaflet-geosearch";
 import VGeosearch from "vue2-leaflet-geosearch";
 import { Icon } from "leaflet";
-
 import Vue2LeafletMarkerCluster from "vue2-leaflet-markercluster";
 
 const DummyLocations = [
-  { lat: 2.4471708623464177, lng: -76.60543462886939 },
-  { lat: 2.4071708623464177, lng: -76.60543462886939 },
-  { lat: 2.4471708623464177, lng: -75.60543462886939 },
-  { lat: 45.3937203599115, lng: 8.544738061411797 },
-  { lat: 47.3744092665564, lng: -95.550949011575966 },
-  { lat: 40.857687895771015, lng: -90.143336159214287 },
-  { lat: 47.3937203599115, lng: 8.544738061411797 },
-  { lat: 47.3744092665564, lng: -98.550949011575966 }
+  { info: "Marker info", lat: 2.4471708623464177, lng: -76.60543462886939 },
+  { info: "Marker info", lat: 2.4071708623464177, lng: -76.60543462886939 },
+  { info: "Marker info", lat: 2.4471708623464177, lng: -75.60543462886939 },
+  { info: "Marker info", lat: 45.3937203599115, lng: 8.544738061411797 },
+  { info: "Marker info", lat: 47.3744092665564, lng: -95.550949011575966 },
+  { info: "Marker info", lat: 40.857687895771015, lng: -90.143336159214287 },
+  { info: "Marker info", lat: 47.3937203599115, lng: 8.544738061411797 },
+  { info: "Marker info", lat: 47.3744092665564, lng: -98.550949011575966 }
 ];
 
 export default {
+  props: {
+    isEditable: { type: Boolean, default: true }
+  },
   components: {
     "v-map": LMap,
     "v-tile-layer": LTileLayer,
     "v-marker": LMarker,
     "v-marker-cluster": Vue2LeafletMarkerCluster,
     "v-control-zoom": LControlZoom,
+    "v-control": LControl,
+    "v-popup": LPopup,
+    "v-polygon": LPolygon,
     VGeosearch
   },
   data() {
     return {
       url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
       attribution:
-        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+        '&copy; <a href="http://osm.org/copyright" target="_blank">OpenStreetMap</a> | <a href="https://locationiq.com/" target="_blank">LocationIQ</a> contributors',
       zoom: 3,
-      center: [47.41322, -1.219482],
+      center: [47.38454197098293, 8.542212126985232],
       bounds: null,
       locations: [],
       geosearchOptions: {
@@ -70,7 +114,14 @@ export default {
         autoClose: true,
         keepResult: false
       },
-      isEdition: false
+      polygon: {
+        latlngs: [],
+        color: "green",
+        fill: true,
+        fillColor: "red"
+      },
+      searchActive: false,
+      toolbar: { editionMode: false, tools: { marker: false, polygon: false } }
     };
   },
   methods: {
@@ -83,35 +134,69 @@ export default {
     boundsUpdated(bounds) {
       this.bounds = bounds;
     },
-    removeMarker(index) {
-      console.log("jajajaja");
-      this.locations.splice(index, 1);
-    },
-    addMarker(event) {
-      this.isEdition = !document
-        .getElementsByClassName("geosearch")[0]
-        .classList.contains("active");
-      if (
-        (event.containerPoint.x >= 10 && event.containerPoint.x <= 40) ||
-        (event.containerPoint.y >= 10 && event.containerPoint.y <= 40)
-      ) {
-        this.isEdition = false;
+    clickOnMarker(location, index) {
+      // Remove marker when edition mode is on
+      if (this.toolbar.editionMode) {
+        this.locations.splice(index, 1);
         return;
       }
-      if (this.isEdition) {
-        this.locations.push(event.latlng);
+    },
+    clickOnPoligon(event) {
+      console.log(event);
+    },
+    clickOnMap(event) {
+      // Add marker when edition mode is on
+      if (this.toolbar.editionMode) {
+        // Get the search geolocation event through div class
+        this.searchActive = document
+          .getElementsByClassName("geosearch")[0]
+          .classList.contains("active");
+        if (
+          (event.containerPoint.x >= 10 && event.containerPoint.x <= 40) ||
+          (event.containerPoint.y >= 10 && event.containerPoint.y <= 40)
+        ) {
+          this.searchActive = true;
+          return;
+        }
+        if (!this.searchActive) {
+          if (this.toolbar.tools.marker) {
+            this.locations.push(event.latlng);
+            return;
+          }
+          if (this.toolbar.tools.polygon) {
+            this.polygon.latlngs.push([event.latlng.lat, event.latlng.lng]);
+            return;
+          }
+        }
       }
     },
-
     setData() {
-      this.locations = DummyLocations.map(x => [x.lat, x.lng]);
+      this.locations = DummyLocations;
+      // TODO: replace DummyLocations for real external data
       const [minLat, maxLat, minLng, maxLng] = [
-        Math.min.apply(Math, this.locations.map(x => x[0])),
-        Math.max.apply(Math, this.locations.map(x => x[0])),
-        Math.min.apply(Math, this.locations.map(x => x[1])),
-        Math.max.apply(Math, this.locations.map(x => x[1]))
+        Math.min.apply(Math, DummyLocations.map(x => x.lat)),
+        Math.max.apply(Math, DummyLocations.map(x => x.lat)),
+        Math.min.apply(Math, DummyLocations.map(x => x.lng)),
+        Math.max.apply(Math, DummyLocations.map(x => x.lng))
       ];
       this.center = [(maxLat + minLat) / 2, (maxLng + minLng) / 2];
+    },
+
+    toggleEditionMode(tool) {
+      for (var t in this.toolbar.tools) {
+        if (t == tool) {
+          this.toolbar.tools[t] = !this.toolbar.tools[t];
+        } else {
+          this.toolbar.tools[t] = false;
+        }
+      }
+      this.toolbar.editionMode = Object.values(this.toolbar.tools).some(
+        x => x == true
+      );
+    },
+
+    getLocation(location) {
+      return `${location.lat}, ${location.lng}`;
     }
   },
   created() {
@@ -127,11 +212,34 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "@/scss/variables.scss";
 .maps {
-  height: 80vh;
-  width: 100vw;
+  height: 90vh;
+  width: 95vw;
   position: relative;
   margin: auto;
   padding-bottom: 100px;
+  &.edition {
+    cursor: pointer;
+  }
+}
+.custom-buttons {
+  bottom: 3%;
+  left: 3%;
+
+  .custom-button {
+    display: block;
+    border-radius: 10%;
+    background: rgba(255, 255, 255, 0.6);
+    height: 3em;
+    width: 3em;
+    box-shadow: 2px 1px 5px $black;
+    &.active {
+      color: $primary;
+    }
+  }
+  .custom-button:hover {
+    background: rgba(255, 255, 255, 0.9);
+  }
 }
 </style>
