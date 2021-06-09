a8a1a70e4d024ee8c4111d9c61e5d84c4a7b7afa
Andres Dorado <andres.dorado90@gmail.com>
2021-06-09 17:22:45 +0200

    Input field for media Zip file and validations in LoadData.vue
    Part of CSLogger integration

--- src/components/Task/Builder/TemplateEditor/CSLogger/LoadData.vue
+++ src/components/Task/Builder/TemplateEditor/CSLogger/LoadData.vue
@@ -1,38 +1,62 @@
 <template>
   <b-form ref="form" @submit.prevent="onSubmit()" class="mt-4">
     <b-form-group>
-      <label>{{ $t("taks-import-cslogger-csv-label") }}</label>
-      <b-file
-        :placeholder="$t('taks-import-cslogger-csv')"
-        accept=".csv"
-        v-model="csvFile"
-        class="mt-3 mb-2"
-        browse-text="Search"
-        autofocus
-        required
-        :state="validateCSV"
-      ></b-file>
-      <span class="text-secondary" v-show="valid">
-        <i class="fas fa-check-circle"></i>
-        <small> {{ $t("taks-import-cslogger-valid") }}</small>
-      </span>
-      <span class="text-primary" v-show="!valid && valid != null">
-        <i class="fas fa-exclamation-triangle"></i>
-        <small>{{ error_message }}</small>
-      </span>
-      <!-- <label>{{ $t("taks-import-cslogger-media-label") }}</label>
-      <b-file
-        :placeholder="$t('taks-import-cslogger-zip')"
-        accept=".zip"
-        v-model="zipfile"
-      ></b-file> -->
+      <!-- Results file in CSV format -->
+      <div class="mb-3">
+        <label>{{ $t("taks-import-cslogger-csv-label") }}</label>
+        <b-file
+          :placeholder="$t('taks-import-cslogger-csv')"
+          accept=".csv"
+          v-model="csvFile"
+          class="mb-1"
+          browse-text="Search"
+          required
+          :state="validateCSV"
+        ></b-file>
+        <span class="text-secondary" v-show="valid.csv">
+          <i class="fas fa-check-circle"></i>
+          <small> {{ $t("taks-import-cslogger-valid") }}</small>
+        </span>
+        <span class="text-primary" v-show="!valid.csv && valid.csv != null">
+          <i class="fas fa-exclamation-triangle"></i>
+          <small>{{ error_message.csv }}</small>
+        </span>
+        <small class="text-muted" v-if="valid.csv == null">
+          <i>Max. size 1Mb</i>
+        </small>
+      </div>
+
+      <!-- Media file in ZIP format -->
+      <div class="mb-4">
+        <label>{{ $t("taks-import-cslogger-media-label") }}</label>
+        <b-file
+          :placeholder="$t('taks-import-cslogger-zip')"
+          accept=".zip"
+          v-model="zipFile"
+          class="mb-1"
+          browse-text="Search"
+          required
+          :state="validateZIP"
+        ></b-file>
+        <span class="text-secondary" v-show="valid.zip">
+          <i class="fas fa-check-circle"></i>
+          <small> {{ $t("taks-import-cslogger-valid") }}</small>
+        </span>
+        <span class="text-primary" v-show="!valid.zip && valid.zip != null">
+          <i class="fas fa-exclamation-triangle"></i>
+          <small>{{ error_message.zip }}</small>
+        </span>
+        <small class="text-muted" v-if="valid.zip == null">
+          <i>Max. size 200Mb</i>
+        </small>
+      </div>
     </b-form-group>
 
-    <div class="mt-3">
+    <div>
       <b-button
         type="submit"
-        variant="secondary"        
-        :disabled="!valid"
+        variant="secondary"
+        :disabled="!isValid"
         class="mr-1"
         >{{ $t("cslogger-load-data") }}</b-button
       >
@@ -58,18 +82,26 @@ export default {
   data() {
     return {
       csvFile: null,
-      zipfile: null,
+      zipFile: null,
       loading: false,
-      valid: null,
+      valid: { csv: null, zip: null },
       loaded: null,
-      error_message: null
+      error_message: { csv: null, zip: null }
     };
   },
   computed: {
     validateCSV() {
-      if (this.valid) return true;
-      if (!this.valid && this.valid != null) return false;
+      if (this.valid.csv) return true;
+      if (!this.valid.csv && this.valid.csv != null) return false;
       return null;
+    },
+    validateZIP() {
+      if (this.valid.zip) return true;
+      if (!this.valid.zip && this.valid.zip != null) return false;
+      return null;
+    },
+    isValid() {
+      return Object.values(this.valid).every(x => x);
     }
   },
   methods: {
@@ -85,29 +117,43 @@ export default {
         this.loaded = "ok";
       }, 3000);
     },
-    next(){
-      this.$emit("onContinue")
+    next() {
+      this.$emit("onContinue");
+    },
+    validate(ext, size) {
+      this.loaded = null;
+      this.valid[ext] = null;
+      this.error_message[ext] = null;
+
+      let file;
+      if (ext === "csv") file = this.csvFile;
+      if (ext === "zip") file = this.zipFile;
+
+      if (file.name.includes(ext)) {
+        this.valid[ext] = true;
+        if (file.size > size) {
+          this.valid[ext] = false;
+          this.error_message[ext] = this.$t(
+            "taks-import-cslogger-invalid-size"
+          );
+        }
+      } else {
+        this.valid[ext] = false;
+        this.error_message[ext] = this.$t(
+          "taks-import-cslogger-invalid-format"
+        );
+      }
     }
   },
   watch: {
     csvFile() {
-      this.valid = null;
-      this.error_message = null;
-      this.loaded = null;
       console.log(this.csvFile);
-      if (this.csvFile.name.includes(".csv")) {
-        this.valid = true;
-        if (this.csvFile.size > 1000000) {
-          this.valid = false;
-          this.error_message = this.$t("taks-import-cslogger-invalid-size");
-        }
-      } else {
-        this.valid = false;
-        this.error_message = this.$t("taks-import-cslogger-invalid-format");
-      }
+      this.validate("csv", 1000000); // 1 Mb
+    },
+    zipFile() {
+      console.log(this.zipFile);
+      this.validate("zip", 200000000); //200 Mb
     }
   }
 };
 </script>
-
-<style></style>
