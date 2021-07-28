c08259bb2d614599df268ab873c3a555d49d95a7
Andres Dorado <andres.dorado@gmail .com>
2021-07-28 18:04:36 +0200

    Info content changed for information, story and summary builder. work in progress

--- src/components/Project/Builder/StoryBuilder.vue
+++ src/components/Project/Builder/StoryBuilder.vue
@@ -2,91 +2,107 @@
   <div>
     <b-row class="mt-4">
       <b-col>
-        <b-link :to="{ name: 'project.builder.name' }"> {{ $t('go-back-btn') }} </b-link>
+        <b-link :to="{ name: 'project.builder.name' }">
+          {{ $t("go-back-btn") }}
+        </b-link>
       </b-col>
     </b-row>
 
     <b-form ref="form" @submit.prevent="onSubmit">
       <b-row>
         <b-col md="9">
-          <h1 class="mt-3 mb-3 small"> {{ $t('story-builder-project-info') }} </h1>
+          <h1 class="mt-3 mb-3 small">
+            {{ $t("story-builder-project-info") }}
+          </h1>
 
           <b-form-group
-                  id="fieldset-what-why"
-                  :label="$t('project-description-what-why')"
-                  label-for="what-why"
-                  :description="$t('story-builder-project-info-description-1')"
-                  :valid-feedback="validFeedback('currentWhatWhy')"
-                  :invalid-feedback="invalidFeedback('currentWhatWhy')"
-                  :state="validated('currentWhatWhy')"
+            id="fieldset-what-why"
+            :label="$t('project-description-what-why')"
+            label-for="what-why"
+            :description="$t('story-builder-project-info-description-1')"
+            :valid-feedback="validFeedback('currentWhatWhy')"
+            :invalid-feedback="invalidFeedback('currentWhatWhy')"
+            :state="validated('currentWhatWhy')"
           >
-            <vue-editor id="what-why" :editorToolbar="toolbarOptions" v-model="currentWhatWhy"></vue-editor>
+            <vue-editor
+              id="what-why"
+              :editorToolbar="toolbarOptions"
+              v-model="currentWhatWhy"
+            ></vue-editor>
           </b-form-group>
 
           <b-form-group
-                  id="fieldset-how"
-                  :label="$t('project-description-how')"
-                  label-for="how"
-                  :description="$t('story-builder-project-info-description-2')"
-                  :valid-feedback="validFeedback('currentHow')"
-                  :invalid-feedback="invalidFeedback('currentHow')"
-                  :state="validated('currentHow')"
+            id="fieldset-how"
+            :label="$t('project-description-how')"
+            label-for="how"
+            :description="$t('story-builder-project-info-description-2')"
+            :valid-feedback="validFeedback('currentHow')"
+            :invalid-feedback="invalidFeedback('currentHow')"
+            :state="validated('currentHow')"
           >
-            <vue-editor id="how" :editorToolbar="toolbarOptions" v-model="currentHow"></vue-editor>
+            <vue-editor
+              id="how"
+              :editorToolbar="toolbarOptions"
+              v-model="currentHow"
+            ></vue-editor>
           </b-form-group>
 
           <b-form-group
-                  id="fieldset-who"
-                  :label="$t('project-description-who')"
-                  label-for="who"
-                  :description="$t('story-builder-project-info-description-3')"
-                  :valid-feedback="validFeedback('currentWho')"
-                  :invalid-feedback="invalidFeedback('currentWho')"
-                  :state="validated('currentWho')"
+            id="fieldset-who"
+            :label="$t('project-description-who')"
+            label-for="who"
+            :description="$t('story-builder-project-info-description-3')"
+            :valid-feedback="validFeedback('currentWho')"
+            :invalid-feedback="invalidFeedback('currentWho')"
+            :state="validated('currentWho')"
           >
-            <vue-editor id="who" :editorToolbar="toolbarOptions" v-model="currentWho"></vue-editor>
+            <vue-editor
+              id="who"
+              :editorToolbar="toolbarOptions"
+              v-model="currentWho"
+            ></vue-editor>
           </b-form-group>
 
           <b-form-group
-                  id="fieldset-keep-track"
-                  :label="$t('project-description-keep-track')"
-                  label-for="keep-track"
-                  :description="$t('story-builder-project-info-description-4')"
-                  :valid-feedback="validFeedback('currentKeepTrack')"
-                  :invalid-feedback="invalidFeedback('currentKeepTrack')"
-                  :state="validated('currentKeepTrack')"
+            id="fieldset-keep-track"
+            :label="$t('project-description-keep-track')"
+            label-for="keep-track"
+            :description="$t('story-builder-project-info-description-4')"
+            :valid-feedback="validFeedback('currentKeepTrack')"
+            :invalid-feedback="invalidFeedback('currentKeepTrack')"
+            :state="validated('currentKeepTrack')"
           >
-            <vue-editor id="keep-track" :editorToolbar="toolbarOptions" v-model="currentKeepTrack"></vue-editor>
+            <vue-editor
+              id="keep-track"
+              :editorToolbar="toolbarOptions"
+              v-model="currentKeepTrack"
+            ></vue-editor>
           </b-form-group>
         </b-col>
-
 
         <b-col md="3" align-self="start">
-          <p><i class="fas fa-info-circle"></i> 
-          <br>
-          {{ $t('story-builder-project-tip') }} <br>
-          
-          </p>
+          <img src="@/assets/infoicon.svg" class="icon-secondary-big mb-3" />
+          <p v-html="$t('story-builder-project-tip')" class="text-muted"></p>
         </b-col>
-
       </b-row>
 
       <b-row class="mt-4">
         <b-col>
-          <b-button type="submit" variant="primary"> {{ $t('continue') }}</b-button>
+          <b-button type="submit" variant="primary">
+            {{ $t("continue") }}</b-button
+          >
         </b-col>
       </b-row>
-
     </b-form>
   </div>
 </template>
 
 <script>
-import { VueEditor } from "vue2-editor"
-import { mapState, mapMutations } from 'vuex'
+import { VueEditor } from "vue2-editor";
+import { mapState, mapMutations } from "vuex";
 
 export default {
-  name: 'StoryBuilder',
+  name: "StoryBuilder",
   components: {
     VueEditor
   },
@@ -94,10 +110,10 @@ export default {
     return {
       maxNbCharacters: 800,
 
-      currentWhatWhy: '',
-      currentHow: '',
-      currentWho: '',
-      currentKeepTrack: '',
+      currentWhatWhy: "",
+      currentHow: "",
+      currentWho: "",
+      currentKeepTrack: "",
 
       firstInteraction: {
         currentWhatWhy: true,
@@ -106,36 +122,36 @@ export default {
         currentKeepTrack: true
       },
 
-      toolbarOptions: [
-        ["bold", "italic", "link"], [{ list: "bullet" }]
-      ]
-    }
+      toolbarOptions: [["bold", "italic", "link"], [{ list: "bullet" }]]
+    };
   },
-  created () {
-    this.currentWhatWhy = this.whatWhy
-    this.currentHow = this.how
-    this.currentWho = this.who
-    this.currentKeepTrack = this.keepTrack
+  created() {
+    this.currentWhatWhy = this.whatWhy;
+    this.currentHow = this.how;
+    this.currentWho = this.who;
+    this.currentKeepTrack = this.keepTrack;
   },
   methods: {
-    ...mapMutations('project/builder', [
-      'setStory',
-      'setStep'
-    ]),
+    ...mapMutations("project/builder", ["setStory", "setStep"]),
 
-    onSubmit () {
-      const fields = ['currentWhatWhy', 'currentHow', 'currentWho', 'currentKeepTrack']
-      let isValid = true
+    onSubmit() {
+      const fields = [
+        "currentWhatWhy",
+        "currentHow",
+        "currentWho",
+        "currentKeepTrack"
+      ];
+      let isValid = true;
 
       for (let field of fields) {
-        this.firstInteraction[field] = false
+        this.firstInteraction[field] = false;
         if (!this.validated(field)) {
-          isValid = false
+          isValid = false;
         }
       }
 
       if (!isValid) {
-        return false
+        return false;
       }
 
       this.setStory({
@@ -143,29 +159,40 @@ export default {
         how: this.currentHow,
         who: this.currentWho,
         keepTrack: this.currentKeepTrack
-      })
+      });
 
-      this.setStep({ step: 'story', value: true })
+      this.setStep({ step: "story", value: true });
     },
 
-    validated (field) {
-      let fieldLength = this.$data[field].replace(/<[^>]*>?/gm, '').length;
-      fieldLength = fieldLength < 0 ? 0 : fieldLength
-      return this.firstInteraction[field] || (fieldLength > 0 && fieldLength <= this.maxNbCharacters)
+    validated(field) {
+      let fieldLength = this.$data[field].replace(/<[^>]*>?/gm, "").length;
+      fieldLength = fieldLength < 0 ? 0 : fieldLength;
+      return (
+        this.firstInteraction[field] ||
+        (fieldLength > 0 && fieldLength <= this.maxNbCharacters)
+      );
     },
-    validFeedback (field) {
-      let fieldLength = this.$data[field].replace(/<[^>]*>?/gm, '').length
-      fieldLength = fieldLength < 0 ? 0 : fieldLength
-      return this.maxNbCharacters - fieldLength + ' ' + this.$t('characters-left')
+    validFeedback(field) {
+      let fieldLength = this.$data[field].replace(/<[^>]*>?/gm, "").length;
+      fieldLength = fieldLength < 0 ? 0 : fieldLength;
+      return (
+        this.maxNbCharacters - fieldLength + " " + this.$t("characters-left")
+      );
     },
-    invalidFeedback (field) {
-      let fieldLength = this.$data[field].replace(/<[^>]*>?/gm, '').length
-      fieldLength = fieldLength < 0 ? 0 : fieldLength
-      return fieldLength === 0 ? this.$t('mandatory-field') : this.$t('field-should-not-exceed') + ' ' + this.maxNbCharacters + ' ' + this.$t('characters')
+    invalidFeedback(field) {
+      let fieldLength = this.$data[field].replace(/<[^>]*>?/gm, "").length;
+      fieldLength = fieldLength < 0 ? 0 : fieldLength;
+      return fieldLength === 0
+        ? this.$t("mandatory-field")
+        : this.$t("field-should-not-exceed") +
+            " " +
+            this.maxNbCharacters +
+            " " +
+            this.$t("characters");
     }
   },
   computed: {
-    ...mapState('project/builder', {
+    ...mapState("project/builder", {
       whatWhy: state => state.story.whatWhy,
       how: state => state.story.how,
       who: state => state.story.who,
@@ -173,22 +200,27 @@ export default {
     })
   },
   watch: {
-    currentWhatWhy () {
-      this.firstInteraction.currentWhatWhy = false
+    currentWhatWhy() {
+      this.firstInteraction.currentWhatWhy = false;
     },
-    currentHow () {
-      this.firstInteraction.currentHow = false
+    currentHow() {
+      this.firstInteraction.currentHow = false;
     },
-    currentWho () {
-      this.firstInteraction.currentWho = false
+    currentWho() {
+      this.firstInteraction.currentWho = false;
     },
-    currentKeepTrack () {
-      this.firstInteraction.currentKeepTrack = false
+    currentKeepTrack() {
+      this.firstInteraction.currentKeepTrack = false;
     }
   }
-}
+};
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+@import "@/scss/variables.scss";
+.icon-secondary-big {
+  color: $secondary;
+  width: 2.5em;
+  height: auto;
+}
 </style>
--- src/components/Project/Builder/SummaryBuilder.vue
+++ src/components/Project/Builder/SummaryBuilder.vue
@@ -6,7 +6,7 @@
         <p>{{ $t('summary-builder-just-created-project') }} <strong>{{ selectedProject.name }}</strong>.</p>
         <p>{{ $t('summary-builder-keep-going') }}</p>
         <b-btn variant="secondary" class="mt-3 mb-3" :to="{ name: 'task.builder.material', params: { id: selectedProject.id } }">{{ $t('summary-builder-comeon') }}</b-btn><br>
-        <i>{{ $t('summary-builder-leave-later') }} <b-link :to="{ name: 'project', params: { id: selectedProject.id } }"> {{ $t('summary-builder-project-page') }} </b-link></i>
+        <!-- <i>{{ $t('summary-builder-leave-later') }} <b-link :to="{ name: 'project', params: { id: selectedProject.id } }"> {{ $t('summary-builder-project-page') }} </b-link></i> -->
       </b-col>
     </b-row>
   </div>
