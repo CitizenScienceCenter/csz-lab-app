20278f27814e28a71ef47008605650dcd4ff62c3
Andres Dorado <andres.dorado90@gmail.com>
2021-10-02 19:04:47 +0200

    Text based component for task presenter

---
+++ src/components/Task/Presenter/Resources/TextBasedTaskPresenter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <b-card-text class="text-left" v-if="type == 'text'">
+      {{ content }}
+    </b-card-text>
+    <b-card-text class="text-left" v-if="type == 'value'">
+      Content: {{ content }} - Options: {{options}}
+    </b-card-text>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextBasedTaskPresenter",
+  props: {
+    type: String,
+    content: String,
+    options: String,
+  },
+};
+</script>
+
+<style></style>
