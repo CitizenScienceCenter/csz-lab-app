7a516d3c47a3f957f804cb55a41155ed5bd09649
Andres Dorado <andres.dorado90@gmail.com>
2021-10-02 19:20:29 +0200

    ValueBasedTaskPresenter.vue created for those responses types which include options
    This component was separated from TextBased, those don't contain options

--- src/components/Task/Presenter/MediaPresenter.vue
+++ src/components/Task/Presenter/MediaPresenter.vue
@@ -21,11 +21,17 @@
     ></maps-task-presenter>
     <!-- For text-based component -->
     <text-based-task-presenter
-      v-else-if="text_based_types.includes(mime)"
+      v-else-if="text_based_types.includes(mime) && link"
+      :content="link"
+      :type="mime"
+    ></text-based-task-presenter>
+    <!-- For types with options component -->
+    <value-based-task-presenter
+      v-else-if="mime == 'value' && link"
       :content="link"
       :options="options"
       :type="mime"
-    ></text-based-task-presenter>
+    ></value-based-task-presenter>
     <!-- No recognized element -->
     <b-alert v-else :show="true" variant="danger">{{
       $t("template-editor-text-16")
@@ -37,17 +43,19 @@
 import MediaTaskPresenter from "./Resources/MediaTaskPresenter";
 import ImageTaskPresenter from "./Resources/ImageTaskPresenter";
 import TextBasedTaskPresenter from "./Resources/TextBasedTaskPresenter";
+import ValueBasedTaskPresenter from "./Resources/ValueBasedTaskPresenter";
 import Maps from "./Resources/Maps";
 import { getMIME } from "@/helper.js";
 
 const MEDIA_TYPES = ["img", "audio", "video", "vembed"];
-const TEXT_BASED_TYPES = ["text", "value", "date", "time_range"];
+const TEXT_BASED_TYPES = ["text", "date", "time_range"];
 export default {
   name: "MediaPresenter",
   components: {
     MediaTaskPresenter,
     ImageTaskPresenter,
     TextBasedTaskPresenter,
+    ValueBasedTaskPresenter,
     "maps-task-presenter": Maps,
   },
   data() {
