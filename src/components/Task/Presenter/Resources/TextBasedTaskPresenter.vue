84b94a4c6afcd911fa6b79ca925d5ab987df87a7
Andres Dorado <andres.dorado90@gmail.com>
2021-10-07 12:29:06 +0200

    Time range added
    All text based components were added to a dynamic component set using component tag

--- src/components/Task/Presenter/Resources/TextBasedTaskPresenter.vue
+++ src/components/Task/Presenter/Resources/TextBasedTaskPresenter.vue
@@ -4,7 +4,7 @@
       {{ content }}
     </b-card-text>
     <b-calendar
-      class="readonly"
+      class="readonly mt-2"
       v-else-if="type == 'date'"
       :value="getDate"
       readonly
@@ -14,7 +14,12 @@
       label-help=""
     >
     </b-calendar>
-    <!-- {{ getDate }} -->
+    <div v-else-if="type == 'time_range'" class="mt-2">
+      <span class="font-weight-bold"> From:</span> {{ getTimeRange.from }}
+      <i class="far fa-clock fa-sm text-primary"></i>
+      <span class="font-weight-bold ml-3">To:</span> {{ getTimeRange.to }}
+      <i class="far fa-clock fa-sm text-primary"></i>
+    </div>
   </div>
 </template>
 
@@ -24,7 +29,7 @@ export default {
   name: "TextBasedTaskPresenter",
   props: {
     type: String,
-    content: String
+    content: String,
   },
   computed: {
     getDate() {
@@ -39,8 +44,32 @@ export default {
       } catch (error) {
         return null;
       }
-    }
-  }
+    },
+    getTimeRange() {
+      try {
+        let time_range_string = this.content.split("time_range:")[1];
+        if (time_range_string) {
+          // Exclusive for CSLogger
+          time_range_string = time_range_string.replaceAll(" ", "");
+          // Validate if CSLogger time_range format is present
+          const time_regex = /from\(.*\)\/to\(.*\)/g;
+          if (!time_regex.test(time_range_string))
+            throw "Wrong time range format";
+          const time_range = time_range_string.split("/").map(function (x) {
+            const components = x.split("(");
+            const key = components[0];
+            const content = components[1].split(")")[0].match(/\d+/g).join(" : ");
+            return { [key]: content };
+          });
+          return time_range.reduce((x, y) => Object.assign(x, y));
+        } else {
+          throw "No time range detected";
+        }
+      } catch (error) {
+        return null;
+      }
+    },
+  },
 };
 </script>
 
