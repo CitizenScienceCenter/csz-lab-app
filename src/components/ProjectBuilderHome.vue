cb98e754284db8ba72b76f4be543f09d3caf297f
Andres Dorado <andres.dorado90@gmail.com>
2021-11-09 13:08:30 +0100

    navbar modified to include new pages in Header.js
    the routes for cslogger and projectbuilderhome were created
    view for cslogger and projectbuilderhome created
    
    Pending: implement content and styles

---
+++ src/components/ProjectBuilderHome.vue
@@ -0,0 +1,16 @@
+<template>
+  <b-container>
+    <!-- Header section -->
+    <b-row class="mt-4 d-flex justify-content-center">
+      <h1 class="centered">{{ $t("projectbuilder-header") }}</h1>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+  name: "ProjectBuilderHome",
+};
+</script>
+
+<style></style>
