aaec32421704c74fcb311134e2ce771cc9a1cf12
Andres Dorado <andres.dorado@gmail .com>
2021-11-15 21:44:01 +0100

    TutorialAccordion changed for SidebarContent
    This change is because the sidebar presents a better style for the component purposes than accordion

---
+++ src/components/Common/SidebarContent.vue
@@ -0,0 +1,199 @@
+<template>
+  <div class="mt-2" role="tablist" v-if="content">
+    <!-- Button for sidebar toggle -->
+    <b-button
+      v-b-toggle.sidebar_steps
+      variant="link"
+      class="steps-button"
+      @click.prevent="openSidebar"
+    >
+      <i class="fas fa-stream fa-lg"></i>
+      <span class="ml-1 font-weight-bold"> Steps</span>
+    </b-button>
+    <!-- Card for content section -->
+    <b-card no-body class="mb-1 border-0">
+      <b-card-header
+        header-tag="header"
+        header-bg-variant="secondary"
+        header-text-variant="white"
+      >
+        <span class="font-weight-bold"> {{ current_tab.title }} </span>
+      </b-card-header>
+      <b-card-body class="full-height">
+        <b-card-text v-if="current_tab.content.rows">
+          <!-- Header: only visible after md screens -->
+          <b-row v-if="current_tab.content.row_header" class="pb-2">
+            <b-col
+              cols="4"
+              v-for="h in current_tab.content.row_header"
+              :key="h"
+              class="d-none d-lg-block"
+            >
+              <h2>{{ h }}</h2>
+            </b-col>
+          </b-row>
+          <!-- Content: rows of content -->
+          <b-row
+            v-for="(r, i) in current_tab.content.rows"
+            :key="i"
+            class="mb-3"
+          >
+            <b-col
+              cols="12"
+              :lg="Math.floor(12 / r.length)"
+              v-for="(col, c_i) in r"
+              :key="c_i"
+            >
+              <div v-if="col">
+                <div v-html="col.text" v-if="col.text"></div>
+                <div @click.prevent="selected_img = col.img">
+                  <b-img-lazy
+                    class="cslogger-img"
+                    v-if="col.img"
+                    :src="getImage(col.img)"
+                    fluid
+                    :alt="col.img"
+                    v-b-modal.modal-img
+                  ></b-img-lazy>
+                </div>
+              </div>
+            </b-col>
+          </b-row>
+        </b-card-text>
+      </b-card-body>
+    </b-card>
+
+    <!-- Sidebar section  -->
+    <b-sidebar
+      id="sidebar_steps"
+      :title="$t('cslogger-create-app-sidebar-title')"
+      shadow
+      right
+      class="full-height"
+    >
+      <div class="py-2">
+        <b-button
+          v-for="(item, index) in content"
+          :key="index"
+          block
+          squared
+          class="text-left"
+          :variant="item.id === current_tab.id ? 'primary' : 'link'"
+          @click="changeTab(item.id)"
+        >
+          <span class="font-weight-bold"> {{ item.title }} </span>
+        </b-button>
+      </div>
+    </b-sidebar>
+
+    <!-- Image in modal section -->
+    <b-modal
+      id="modal-img"
+      centered
+      cancel-disabled
+      ok-disabled
+      hide-header
+      hide-footer
+    >
+      <b-img-lazy
+        class="cslogger-img-zoom"
+        :src="getImage(selected_img)"
+        fluid-grow
+        :alt="selected_img"
+      ></b-img-lazy>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SidebarContent",
+  props: { content: Array },
+  data() {
+    return {
+      current_tab: null,
+      selected_img: null
+    };
+  },
+  created() {
+    if (this.content && this.content.length > 0) {
+      this.current_tab = this.content[0];
+    }
+  },
+  methods: {
+    changeTab(id) {
+      this.current_tab = this.content.find(x => x.id === id);
+      this.$root.$emit("bv::toggle::collapse", "sidebar_steps");
+    },
+    openSidebar() {
+      this.$emit("openSidebar");
+    },
+    getImage(img_url) {
+      if (img_url) {
+        return require(`@/assets/img/${img_url}`);
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/scss/variables.scss";
+
+.cslogger-img {
+  max-height: 300px;
+  height: 100%;
+}
+.cslogger-img-zoom {
+  height: auto;
+  max-width: 100%;
+}
+.steps-button {
+  position: absolute;
+  top: 2%;
+  right: 1%;
+  span {
+    display: none;
+  }
+}
+.full-height {
+  max-height: 70vh;
+  overflow: auto;
+}
+@media only screen and (min-width: $viewport-tablet-portrait) {
+  .steps-button {
+    span {
+      display: inline;
+    }
+  }
+}
+@media only screen and (min-width: $viewport-large) {
+  .cslogger-img-zoom {
+    height: auto;
+    max-width: 100%;
+    transform: scale(1.3);
+  }
+  .cslogger-img {
+    max-height: 350px;
+    height: 100%;
+  }
+  .steps-button {
+    top: 3%;
+    right: 2.5%;
+    span {
+      font-size: 1.2rem;
+    }
+  }
+}
+@media only screen and (min-width: $viewport-xxlarge) {
+  .cslogger-img-zoom {
+    height: auto;
+    max-width: 100%;
+    transform: scale(2);
+  }
+  .cslogger-img {
+    max-height: 400px;
+    height: 100%;
+  }
+}
+</style>
--- src/components/Tools/CitizenScienceLogger.vue
+++ src/components/Tools/CitizenScienceLogger.vue
@@ -1,120 +1,136 @@
 <template>
   <div class="cslogger">
     <!-- Header section -->
-    <b-container fluid class="small-bottom scroll-effect">
-      <b-row class="mt-4 d-flex justify-content-center">
-        <b-col cols="10" class="text-center">
-          <h1 class="centered">{{ $t("cslogger-header") }}</h1>
-        </b-col>
-        <b-col cols="12" class="text-center mt-4">
-          <h2 class="centered">{{ $t("cslogger-subheader") }}</h2>
-        </b-col>
-      </b-row>
-    </b-container>
-
-    <!-- First text content section -->
-    <b-container class="scroll-effect scroll-effect-delayed-1">
-      <b-row>
-        <!-- Left column -->
-        <b-col cols="12" md="6" class="pr-md-4 mb-4 mb-md-0">
-          <div
-            class="text-justify"
-            v-html="$t('cslogger-introduction-left-column')"
-          ></div>
-          <!-- anchors -->
-          <div class="text-left mt-2">
-            <b-button-group vertical>
-              <a
-                v-for="(ref, i) in anchors"
-                :key="i"
-                variant="link"
-                @click="gotoAnchor(ref)"
-                class="anchor mb-1"
-              >
-                {{ $t("cslogger-introduction-anchor-" + i) }}
-              </a>
-            </b-button-group>
-          </div>
-        </b-col>
-        <!-- Right column -->
-        <b-col cols="12" md="6" class="text-center pl-md-4">
-          <div
-            class="text-justify"
-            v-html="$t('cslogger-introduction-right-column')"
-          ></div>
-          <b-button
-            variant="primary"
-            href="https://admin.mindlogger.org/"
-            target="_blank"
-            class="mt-5"
-          >
-            {{ $t("cslogger-createapp-button") }}
-          </b-button>
-        </b-col>
-      </b-row>
-    </b-container>
-
-    <!-- Second text integrants section -->
-    <b-container fluid class="superlight-greyish small-bottom" ref="logos">
-      <b-container class="py-4 scroll-effect scroll-effect-delayed-2">
-        <div class="text-justify" v-html="$t('cslogger-team-content')"></div>
-        <!-- Logos -->
-        <b-row class="d-flex justify-content-center mt-5">
-          <a
-            v-for="(logo, i) in team_logos"
-            :key="i"
-            variant="link"
-            :class="`scroll-effect scroll-effect-delayed-${i + 2}`"
-            :href="logo.url"
-            target="_blank"
-          >
-            <b-img
-              class="logo m-2 mx-4"
-              :src="logo.img"
-              :alt="logo.alt"
-              rounded
-              fluid
-            ></b-img>
-          </a>
+    <content-section>
+      <b-container fluid class="small-bottom scroll-effect">
+        <b-row class="mt-4 d-flex justify-content-center">
+          <b-col cols="10" class="text-center">
+            <h1 class="centered">{{ $t("cslogger-header") }}</h1>
+          </b-col>
+          <b-col cols="12" class="text-center mt-4">
+            <h2 class="centered">{{ $t("cslogger-subheader") }}</h2>
+          </b-col>
         </b-row>
       </b-container>
-    </b-container>
+    </content-section>
+
+    <!-- First text content section -->
+    <content-section>
+      <b-container class="small-bottom scroll-effect scroll-effect-delayed-1">
+        <b-row>
+          <!-- Left column -->
+          <b-col cols="12" md="6" class="pr-md-4 mb-4 mb-md-0">
+            <div
+              class="text-justify"
+              v-html="$t('cslogger-introduction-left-column')"
+            ></div>
+            <!-- anchors -->
+            <div class="text-left mt-2">
+              <b-button-group vertical>
+                <a
+                  v-for="(ref, i) in anchors"
+                  :key="i"
+                  variant="link"
+                  @click="gotoAnchor(ref)"
+                  class="anchor mb-1 font-weight-bold"
+                >
+                  {{ $t("cslogger-introduction-anchor-" + i) }}
+                </a>
+              </b-button-group>
+            </div>
+          </b-col>
+          <!-- Right column -->
+          <b-col cols="12" md="6" class="text-center pl-md-4">
+            <div
+              class="text-justify"
+              v-html="$t('cslogger-introduction-right-column')"
+            ></div>
+            <b-button
+              variant="primary"
+              href="https://admin.mindlogger.org/"
+              target="_blank"
+              class="mt-5"
+            >
+              {{ $t("cslogger-createapp-button") }}
+            </b-button>
+          </b-col>
+        </b-row>
+      </b-container>
+    </content-section>
+
+    <!-- Second text integrants section -->
+    <content-section>
+      <b-container fluid class="superlight-greyish small-bottom" ref="logos">
+        <b-container class="py-4 scroll-effect scroll-effect-delayed-2">
+          <div class="text-justify" v-html="$t('cslogger-team-content')"></div>
+          <!-- Logos -->
+          <b-row class="d-flex justify-content-center mt-5">
+            <a
+              v-for="(logo, i) in team_logos"
+              :key="i"
+              variant="link"
+              :class="`scroll-effect scroll-effect-delayed-${i + 2}`"
+              :href="logo.url"
+              target="_blank"
+            >
+              <b-img
+                class="logo m-2 mx-2 mx-sm-4"
+                :src="logo.img"
+                :alt="logo.alt"
+                rounded
+                fluid
+              ></b-img>
+            </a>
+          </b-row>
+        </b-container>
+      </b-container>
+    </content-section>
 
     <!-- How to create an App section -->
-    <b-container
-      fluid
-      class="full-heigth small-bottom scroll-effect scroll-effect-delayed-1 pt-4 px-0 px-md-2 px-xl-5"
-      ref="create_app"
-    >
-      <b-row>
-        <b-col cols="12">
-          <h1 class="small pb-2 mb-1">{{ $t("cslogger-create-app-header") }}</h1>
-        </b-col>
-      </b-row>
-      <tutorial-accordion :content="createAppContent"></tutorial-accordion>
-    </b-container>
+    <content-section>
+      <b-container
+        fluid
+        class="full-heigth small-bottom scroll-effect scroll-effect-delayed-1 pt-4 px-0 px-md-2 px-xl-5"
+        ref="create_app"
+      >
+        <b-row>
+          <b-col cols="12">
+            <h1 class="small pb-2 mb-1">
+              {{ $t("cslogger-create-app-header") }}
+            </h1>
+          </b-col>
+        </b-row>
+        <sidebar-content
+          :content="createAppContent"
+          @openSidebar="gotoAnchor('create_app')"
+        ></sidebar-content>
+      </b-container>
+    </content-section>
 
     <hr class="mx-2" />
 
     <!-- How to share an App section -->
-    <b-container
-      fluid
-      class="full-heigth small-bottom scroll-effect scroll-effect-delayed-1 pt-4 px-0 px-md-2 px-xl-5"
-      ref="share_app"
-    >
-      <b-row>
-        <b-col cols="12">
-          <h1>{{ $t("cslogger-share-app-header") }}</h1>
-        </b-col>
-      </b-row>
-      <tutorial-accordion></tutorial-accordion>
-    </b-container>
+    <content-section>
+      <b-container
+        fluid
+        class="full-heigth small-bottom scroll-effect scroll-effect-delayed-1 pt-4 px-0 px-md-2 px-xl-5"
+        ref="share_app"
+      >
+        <b-row>
+          <b-col cols="12">
+            <h1>{{ $t("cslogger-share-app-header") }}</h1>
+          </b-col>
+        </b-row>
+        <sidebar-content></sidebar-content>
+      </b-container>
+    </content-section>
   </div>
 </template>
 
 <script>
 import { throttle } from "lodash";
-import TutorialAccordion from "@/components/Common/TutorialAccordion";
+import SidebarContent from "@/components/Common/SidebarContent";
+import ContentSection from "@/components/Common/ContentSection";
 
 let ctrl_scroll = 0;
 const CREATE_APP_CONTENT = require("@/assets/cslogger_view/create_app.json");
@@ -122,14 +138,15 @@ const CREATE_APP_CONTENT = require("@/assets/cslogger_view/create_app.json");
 export default {
   name: "CitizenScienceLogger",
   components: {
-    TutorialAccordion,
+    ContentSection,
+    SidebarContent
   },
   data() {
     return {
       team_logos: [],
       anchors: ["create_app", "share_app", "integration_pb"],
       throttleScroll: throttle(this.handleScroll, 300),
-      createAppContent: CREATE_APP_CONTENT,
+      createAppContent: CREATE_APP_CONTENT
     };
   },
   created() {
@@ -138,20 +155,13 @@ export default {
   },
   beforeMount() {
     // auto scroll to the page top when render first time
-    setTimeout(function () {
+    setTimeout(function() {
       window.scrollTo({ top: 0, behavior: "smooth" });
     }, 2);
   },
   mounted() {
     // Initialize the scroll control
     ctrl_scroll = 0;
-    // Set the initial transition effects
-    let matches = this.$el.querySelectorAll(".scroll-effect");
-    window.setTimeout(function () {
-      for (let item of matches) {
-        item.classList.add("scrolled");
-      }
-    }, 1);
   },
   beforeDestroy() {
     window.removeEventListener("scroll", this.throttleScroll, false);
@@ -167,13 +177,13 @@ export default {
         {
           alt: "child_mind_inst_logo",
           img: child_mind_inst_logo,
-          url: "https://childmind.org/",
+          url: "https://childmind.org/"
         },
         {
           alt: "eth_lib_lab_logo",
           img: eth_lib_lab_logo,
-          url: "https://www.librarylab.ethz.ch/",
-        },
+          url: "https://www.librarylab.ethz.ch/"
+        }
       ];
     },
 
@@ -194,11 +204,11 @@ export default {
       let scroll_down = ctrl_scroll < screen_pos;
       const create_app = {
         offsetTop: this.$refs[this.anchors[0]].offsetTop,
-        scrollHeight: this.$refs[this.anchors[0]].scrollHeight,
+        scrollHeight: this.$refs[this.anchors[0]].scrollHeight
       };
       const share_app = {
         offsetTop: this.$refs[this.anchors[1]].offsetTop,
-        scrollHeight: this.$refs[this.anchors[1]].scrollHeight,
+        scrollHeight: this.$refs[this.anchors[1]].scrollHeight
       };
       if (scroll_down) {
         if (
@@ -218,8 +228,8 @@ export default {
         }
       }
       ctrl_scroll = screen_pos;
-    },
-  },
+    }
+  }
 };
 </script>
 
@@ -232,7 +242,7 @@ export default {
   }
 }
 .logo {
-  max-height: 100px;
+  max-height: 60px;
   cursor: pointer;
   transform: scale(1);
   transition: all $transition-duration-short ease-in-out;
@@ -241,6 +251,16 @@ export default {
   }
 }
 .full-heigth {
-  height: 95vh;
+  min-height: 90vh;
+}
+@media only screen and (min-width: $viewport-tablet-portrait) {
+  .logo {
+    max-height: 80px;
+  }
+}
+@media only screen and (min-width: $viewport-large) {
+  .logo {
+    max-height: 100px;
+  }
 }
 </style>
