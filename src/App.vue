8b6d28380906a9a27db9c4a621fdeb5b044424c6
Andres Dorado <andres.dorado90@gmail.com>
2021-12-06 17:26:29 +0100

    Due to the asynchronous loading of store the gtag was disabled
    Issue solved in App.vue, when the gtag state is in tru, the gtag is bootstraped

--- src/App.vue
+++ src/App.vue
@@ -63,9 +63,11 @@
 </template>
 
 <script>
-import { mapState, mapMutations, mapActions } from "vuex";
+import { mapState, mapMutations, mapActions, mapGetters } from "vuex";
 import { i18n } from "./i18n";
 import Loading from "@/components/Common/Loading.vue";
+// modules for managing gtag
+import { bootstrap } from "vue-gtag";
 
 import GDPR from "./components/GDPR.vue";
 import Footer from "./components/Footer.vue";
@@ -130,6 +132,7 @@ export default {
       project: state => state.project.selectedProject,
       showProjectPassModal: state => state.project.showProjectPassModal
     }),
+    ...mapGetters({ getGtag: "settings/getGtag" }),
     language: {
       get() {
         return this.$store.state.settings.language;
@@ -139,7 +142,11 @@ export default {
       }
     }
   },
-  watch: {},
+  watch: {
+    getGtag() {
+      if (this.getGtag) bootstrap().then(gtag => {});
+    }
+  },
   methods: {
     ...mapMutations({
       closeError: "notification/closeError",
