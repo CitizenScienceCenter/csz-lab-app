4c1c52e1f43d955483ce94a9f7b0cf4407e6f8c7
Andres Dorado <andres.dorado90@gmail.com>
2022-03-28 23:16:18 +0200

    Revert "Skip and Submit methods associated between template and GeoTwitter component"
    This reverts commit 0371bb8cc0590f669b656462d6ed5c7da23e6c89.

--- config/dev.env.js
+++ config/dev.env.js
@@ -7,6 +7,5 @@ module.exports = merge(prodEnv, {
   BASE_ENDPOINT_URL: '"https://pybossa-staging.citizenscience.ch/"',
   BASE_API_URL: '"https://pybossa-staging.citizenscience.ch/api/"',
   FLICKR_CALLBACK_URL: '"http://localhost:8080/flickr/callback"',
-  GTAG_ID: '""',
-  GOOGLE:'"AIzaSyAqBvKF0AR7Rhyyw742m_CJtNDl05JrdpU"'
+  GTAG_ID: '""'
 });
