0371bb8cc0590f669b656462d6ed5c7da23e6c89
Andres Dorado <andres.dorado90@gmail.com>
2022-03-28 23:16:18 +0200

    Skip and Submit methods associated between template and GeoTwitter component

--- config/dev.env.js
+++ config/dev.env.js
@@ -7,5 +7,6 @@ module.exports = merge(prodEnv, {
   BASE_ENDPOINT_URL: '"https://pybossa-staging.citizenscience.ch/"',
   BASE_API_URL: '"https://pybossa-staging.citizenscience.ch/api/"',
   FLICKR_CALLBACK_URL: '"http://localhost:8080/flickr/callback"',
-  GTAG_ID: '""'
+  GTAG_ID: '""',
+  GOOGLE:'"AIzaSyAqBvKF0AR7Rhyyw742m_CJtNDl05JrdpU"'
 });
